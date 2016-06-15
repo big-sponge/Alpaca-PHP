bed9d4284912c5caaa04259da73861c1da0c7ce8
lideshun123 <584309598@qq.com>
2016-06-15 16:53:33 +0800

    merge

--- Source/application/module/Index/Controller/PassportController.php
+++ Source/application/module/Index/Controller/PassportController.php
@@ -5,7 +5,7 @@ use Alpaca\MVC\View\ViewModel;
 use Illuminate\Container\Container;  
 use Illuminate\Database\Capsule\Manager as Capsule;//如果你不喜欢这个名称，as DB;就好 
 use Model\User;
-use Service\JwtAuth\jwtManager;
+use Service\JwtAuth\JwtManager;
  
 class PassportController
 {   
@@ -20,16 +20,24 @@ class PassportController
  
     
     public function indexAction()
-    {   
+    {       
         
+        if (empty($this->params)||empty($this->params[0])) {
 
+            $this->return_data['return_code'] = 0;
+            $this->return_data['return_message'] = "账户必须填写";
+            return ViewModel::json($this->return_data); 
+        }
 
+        $user_name = $this->params[0];
+     
         $data =array(
-            'issuer'=>1,
-            'audience'=>'pay.1010shuju.com',
-            'id'=>111111,
+            'issuer'=> $user_name,
+            'audience'=>$_SERVER["HTTP_HOST"],
+            'id'=>mt_rand(0,100000)*mt_rand(0,100000),
         );
-        $token = jwtManager::jwt()->creatToken($data);
+
+        $token = JwtManager::jwt()->creatToken($data);
         
         $toekn = $token->getToeknString();
         
@@ -46,17 +54,28 @@ class PassportController
         
 
         //获取token
-        $token = $this->params[0];
-        $tokenInfo = jwtManager::jwt()->parserToekn($token);
-         
-        if ($tokenInfo) {
+        $token = $_GET['token'];
 
+       
+
+        $tokenInfo = jwtManager::jwt()->parserToekn($token);
+
+
+        var_dump($tokenInfo);
+        die();
+         
+        if (!$tokenInfo) {
             $this->return_data['return_code'] = 0;
             $this->return_data['return_message'] = "token不存在或者过期";
             return ViewModel::json($this->return_data);
         }
-         
 
+
+        $this->return_data['return_code'] = 1;
+        $this->return_data['return_message'] = "已经登录";
+        $this->return_data['return_toekn'] = $tokenInfo->getClaim('issuer');
+
+        return ViewModel::json($this->return_data);
 
        
     }
