72fd6c8f96cf1df3474a840627ba4ee13714e61d
[cheng] <c-s--c@163.com>
2016-06-17 15:40:49 +0800

    update index

--- Source/application/module/Index/Controller/IndexController.php
+++ Source/application/module/Index/Controller/IndexController.php
@@ -1,19 +1,14 @@
 <?php
 namespace Index\Controller;
 
-use Alpaca\MVC\View\ViewModel;
-use Illuminate\Container\Container;  
-use Illuminate\Database\Capsule\Manager as Capsule;//如果你不喜欢这个名称，as DB;就好 
-use Alpaca\Tools\Validate;
-use Index\Form\PassportForm;
-use Alpaca\Factory\ServerManager;
+use Alpaca\MVC\View\View;
 
 class IndexController
 {               
     public function indexAction()
     {        
         die('Welcome !');         
-        return ViewModel::json();
+        return View::json();
     }    
 }
 
