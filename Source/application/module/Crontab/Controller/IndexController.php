ff8325ddc6fd8fcc2ca7726f108578a03d976ee2
[cheng] <c-s--c@163.com>
2016-06-27 11:44:25 +0800

    定时任务

--- Source/application/module/Crontab/Controller/IndexController.php
+++ Source/application/module/Crontab/Controller/IndexController.php
@@ -71,15 +71,14 @@ class IndexController
                 break;
             }           
             
-            $data->message = date("Y-m-d H:i:s" ,time())." : Working ...";
-            
+            $data->message = date("Y-m-d H:i:s" ,time())." : Working ...";            
             file_put_contents($this->crontab_json, json_encode($data), LOCK_EX);
             sleep(2);                        
         }
+        
         $this->stop();
         
-        Log::add("End Work ...");
-        
+        Log::add("End Work ...");       
         die("Exit:".date("Y-m-d H:i:s" ,time()));
     }
     
@@ -87,58 +86,58 @@ class IndexController
     //异步调用
     public function workerAction()
     {
-        
-        $data = json_decode(file_get_contents($this->crontab_json));
-
-        var_dump($data);
-
-        die('worker!');
-        
-        $fp = fsockopen("192.168.1.15", 8081, $errno, $errstr, 1);
-        if (!$fp) {
-            echo "$errstr ($errno)<br />\n";
-        } else {
-        
-            $out = "POST /auto/checkGroupAccountCange HTTP/1.1\r\n";
-            $out .= "Host: 192.168.1.15\r\n";
-            $out .= "Connection: Close\r\n\r\n";
-            $out .="\r\n";
-            $out .="{'data':111}";
-            fwrite($fp, $out);
-            fclose($fp);
-        }
-        
+        $this->doWorker(['REQUEST_DTAT'=>"ccc",'REQUEST_URI'=>"/crontab/worker/index",]);
         die('worker!');
     }
     
-    private function doWorkerAction(array $worker)
+    private function doWorker(array $worker = null)
     {
-                
-        $ip   = empty($worker['SERVER_ADDR']) ? '127.0.0.1' : $worker['SERVER_ADDR'];       //服务器IP地址
-        
-        $port = empty($worker['SERVER_PORT']) ? '80' : $worker['SERVER_PORT'];              //服务器端口
-        
-        $url  = $worker['REQUEST_URI'];                                                     //服务器URL
-        
-        die('worker!');
-    
-        $fp = fsockopen("192.168.1.15", 8081, $errno, $errstr, 1);
+        $ip   = empty($worker['SERVER_ADDR']) ? $_SERVER['SERVER_ADDR'] : $worker['SERVER_ADDR'];     //服务器IP地址       
+        $port = empty($worker['SERVER_PORT']) ? $_SERVER['SERVER_PORT'] : $worker['SERVER_PORT'];     //服务器端口       
+        $url  = empty($worker['REQUEST_URI']) ? '/' :$worker['REQUEST_URI'];                          //服务器URL
+        $data = empty($worker['REQUEST_DTAT']) ? '' :$worker['REQUEST_DTAT'];                         //请求参数
+
+        $fp = fsockopen("{$ip}", $port, $errno, $errstr, 1);
         if (!$fp) {
             echo "$errstr ($errno)<br />\n";
         } else {
-    
-            $out = "POST /auto/checkGroupAccountCange HTTP/1.1\r\n";
-            $out .= "Host: 192.168.1.15\r\n";
+            $out = "POST {$url} HTTP/1.1\r\n";
+            $out .= "Host: {$ip}\r\n";
             $out .= "Connection: Close\r\n\r\n";
             $out .="\r\n";
-            $out .="{'data':111}";
+            $out .=$data;
             fwrite($fp, $out);
             fclose($fp);
         }
+        return 'worker!';
+    }
     
+    //定时任务
+    public function taskAction()
+    {
+        $task = array(
+            'ID'=>'',           //ID
+            'STATUS'=>'',       // 1-ENABLED,   2-DISABLE  
+            'TYPE'=>'',         // 1-ONCE,      2-LOOP
+            'INTERVAL'=>'',     //year（年），month（月），hour（小时）minute（分），second（秒）
+            'BEGIN_TIME'=>'',   //开始时间
+            'NEXT_TIME'=>'',    //下次执行时间
+            'LAST_TIME'=>'',    //上次执行时间
+            'ACTION'=>'',       //执行的ACTION
+        );
+        
+        $tasks = array();
+        
+        foreach ($tasks as $task){
+            
+        }
+        
+        
+        echo "今天:",date('Y-m-d H:i:s'),"<br>";
+        echo "明天:",date('Y-m-d H:i:s',strtotime('+5 minute'));
+                
         die('worker!');
     }
-         
 }
 
  
---
+++ Source/application/module/Crontab/Controller/WorkerController.php
@@ -0,0 +1,27 @@
+<?php
+namespace Crontab\Controller;
+
+use Alpaca\MVC\View\View;
+use Alpaca\Log\Log;
+
+class WorkerController
+{
+    public function init()
+    {
+        ignore_user_abort(true); // 忽略客户端断开
+        set_time_limit(0);       // 设置执行不超时
+    }
+
+    //index
+    public function indexAction()
+    {
+        Log::add("Start Work ... Welcome Worker index A"); 
+        sleep(5);
+        Log::add("Start Work ... Welcome Worker index B");
+        
+        die('Welcome Worker index !'); 
+        return View::json();
+    }   
+}
+
+ 
